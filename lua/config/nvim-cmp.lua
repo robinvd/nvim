38b8f216a7160914bcdd2b4e2cf62cfa301b111c
Robin <robinvandijk@klippa.com>
2022-05-09 10:57:44 +0200

    add removed defaults to cmp-nvim

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,13 +1,54 @@
 local cmp = require "cmp"
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 cmp.setup {
     snippet = {
         expand = function(args)
             require("luasnip").lsp_expand(args.body)
         end,
     },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
     mapping = {
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<CR>"] = cmp.mapping.confirm { select = false },
+        ['<C-n>'] = cmp.mapping({
+            c = function()
+                if cmp.visible() then
+                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                else
+                    vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+                end
+            end,
+            i = function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                else
+                    fallback()
+                end
+            end
+        }),
+        ['<C-p>'] = cmp.mapping({
+            c = function()
+                if cmp.visible() then
+                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+                else
+                    vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+                end
+            end,
+            i = function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+                else
+                    fallback()
+                end
+            end
+        }),
     },
     sources = {
         { name = "nvim_lsp" },
@@ -17,11 +58,9 @@ cmp.setup {
         { name = "buffer", keyword_length = 4 },
         { name = "cmdline" },
     },
-    -- TODO
     formatting = {
         format = require("lspkind").cmp_format {
             with_text = true,
         },
     },
 }
-print "cmp setup done"
